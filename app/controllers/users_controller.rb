81db783bc964504a68ba754f67cd4b21c35091e1
André Baudin-Laurencin <abaudin@gmail.com>
2014-09-03 21:17:13 +0200

    Rajout de user et debut session from scrath

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,37 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user, notice: 'Thanks for signing up!'
+    else
+      render :new
+    end
+   end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+
+  private
+  def user_params
+    params.require(:user).
+        permit(:name, :email, :password, :password_confirmation, :username, :admin)
+  end
+
+
+
+
+
+
 end
