958550bb1022b7a3d985db98abfdb024cf35fbbe
André Baudin-Laurencin <abaudin@gmail.com>
2014-09-17 09:30:12 +0200

    rajout de agence.raison_sociale dans users/index pour admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
     unless current_user_admin?
       redirect_to root_url, alert: "Unauthorized access!"
     end
-  end
+      end
 
 
   def current_user_admin?
